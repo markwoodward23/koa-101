8e5a4d9e34ffd348b532cdcc5cb69bf3535cd314
Mark Woodward <mwoodward23@hotmail.co.uk>
2020-10-27 08:18:54 +0000

    feat: configurable host

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const proxy = require('koa-proxy');
+const axios = require('axios');
  
 const app = new Koa();
 const router = new Router();
- 
-router.get('/', async (ctx, next) => {
-  console.log('beep boop beep');
-  ctx.status = 200;
-  ctx.body = {
-    data: "wow"
+
+let cache = {};
+
+const proxyHost = process.env.PROXY_HOST 
+
+app.use(async (ctx, next) => {
+  console.log(ctx.request.url);
+  if (cache[ctx.request.url]) {
+    console.log('SERVING CACHED DATA FOR' + ctx.request.url);
+    ctx.body = cache[ctx.request.url]
+  } else {
+    try {
+      if (!/\.(js|css|json|ico|txt)/.test(ctx.request.url)) {
+        const res = await axios.get(`${proxyHost}${ctx.request.url}`);
+        ctx.body = res.data;
+        ctx.status = 200;
+        cache[ctx.request.url] = res.data;
+      } else {
+        console.log('awaiting next');
+        console.log(`${proxyHost}${ctx.request.url}`);
+        await next();
+      }
+    } catch (e) {
+      console.log(e);
+    }
   }
 });
- 
-app.use(router.routes()).use(router.allowedMethods());
+
+app.use(proxy({
+  host: proxyHost 
+}));
 
 app.listen(3000, () => {
   console.log('🚀');
 });
+
