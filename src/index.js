317ab95e81eec7b1502b1ad2e7a8fa94cbc0513f
Mark Woodward <mwoodward23@hotmail.co.uk>
2020-11-09 13:29:45 +0000

    static

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 const Koa = require('koa');
 const Router = require('koa-router');
- 
+const serve = require('koa-static');
+
 const app = new Koa();
-const router = new Router();
- 
-router.get('/', async (ctx, next) => {
-  console.log('beep boop beep');
-  ctx.status = 200;
-  ctx.body = {
-    data: "wow"
-  }
-});
- 
-app.use(router.routes()).use(router.allowedMethods());
+
+app.use(serve('./public'));
 
 app.listen(3000, () => {
   console.log('🚀');
